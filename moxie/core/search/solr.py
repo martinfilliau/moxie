9db7db774acf3d84796225654e58d452c95e6b40
David King <dave@davbo.org>
2012-08-21 14:52:11 +0100

    Added index method

--- moxie/core/search/solr.py
+++ moxie/core/search/solr.py
@@ -9,48 +9,50 @@ logger = logging.getLogger(name=__name__)
 
 class SolrSearch(AbstractSearch):
 
-    UPDATE = "update/"
-    SEARCH = "select/"
     DEFAULT_TIMEOUT = 1     # default timeout in seconds
 
     def __init__(self, core, return_type='json'):
         self.server_url = 'http://localhost:8983/solr/'
         self.core = core
         self.return_type = return_type
+        # Default methods and paths to on Solr
+        self.methods = {'update': 'update/', 'select': 'select/'}
+        self.content_types = {'json': 'application/json'}
         super(SolrSearch, self).__init__(core)
 
     def search(self, query):
-        results = self.connection(self.SEARCH, data=query)
+        results = self.connection(self.methods['search'], data=query)
         return results
 
     def index(self, document):
-        pass
+        params = {'commit': 'true'}
+        results = self.connection(self.methods['update'], params=params,
+                data=document)
 
     def commit(self):
-        pass
+        raise NotImplemented()
 
     def search_for_id(self, id):
         query = {'q': 'identifiers:{0}'.format(self.solr_escape(id))}
         return self.search(query)
 
-    def connection(self, method, params=None, data=None):
+    def connection(self, method, params=None, data=None, headers=None):
         """
         Does a GET request if there is no data otherwise a POST
         @param params GET parameters
         @param data POST form
         """
         if not params:
-            params = {}
+            params = dict()
         if data:
             data = urlencode(data)
         params['wt'] = self.return_type
-        url = '{0}{1}/{2}{3}'.format(self.server_url, self.core, method,
-                                     urlencode(params))
+        url = '{0}{1}/{2}'.format(self.server_url, self.core, method)
         logger.debug(url)
         if data:
-            return post(url, data, timeout=self.DEFAULT_TIMEOUT)
+            return post(url, data, params=params, timeout=self.DEFAULT_TIMEOUT)
         else:
-            return get(url, timeout=self.DEFAULT_TIMEOUT)
+            return get(url, params=params, timeout=self.DEFAULT_TIMEOUT)
 
     @staticmethod
     def solr_escape(string):
