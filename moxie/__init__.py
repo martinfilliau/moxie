ce9fca5d64555d8d4770d4af5eb627b904cef49c
David King <dave@davbo.org>
2012-09-04 15:58:55 +0100

    App now created with an application factory, this helps during testing.

--- moxie/__init__.py
+++ moxie/__init__.py
@@ -2,5 +2,13 @@ from flask import Flask
 from moxie.places import places
 
 
-app = Flask(__name__, static_folder='core/static', template_folder='core/templates')
-app.register_blueprint(places, url_prefix='/places')
+def create_app():
+    app = Flask(__name__, static_folder='core/static', template_folder='core/templates')
+    app.config.from_object('moxie.default_settings')
+    try:
+        app.config.from_envvar('MOXIE_SETTINGS')
+    except RuntimeError:
+        # Env variable not set.
+        pass
+    app.register_blueprint(places, url_prefix='/places')
+    return app
