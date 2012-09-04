60916da316c36cb4f70f887e4d634003559fac0c
David King <dave@davbo.org>
2012-09-04 12:32:56 +0100

    Added a SEARCHER_URL configuration option. Set the indexer based on a url scheme. Trying to mirror the way database url's are configured in sqlalchemy.

--- moxie/core/search/__init__.py
+++ moxie/core/search/__init__.py
@@ -1,3 +1,37 @@
+import urlparse
+import importlib
+
+from flask import _app_ctx_stack
+
+SEARCH_SCHEMES = {
+        'solr': ('moxie.core.search.solr', 'SolrSearch'),
+        }
+
+
+def get_searcher():
+    ctx = _app_ctx_stack.top
+    searcher = getattr(ctx, 'moxie_searcher', None)
+    if searcher is None:
+        searcher_url = ctx.app.config.get('SEARCHER_URL',
+            'solr+http://localhost:8983/solr/collection1')  # Defaults to Solr
+        ctx.moxie_searcher = _find_searcher(searcher_url)
+    return searcher
+
+
+def _find_searcher(searcher_url):
+    parsed = urlparse.urlparse(searcher_url)
+    search_scheme, http_scheme = parsed.scheme.split('+')
+    http_path, _, index_name = parsed.path.rpartition('/')
+    parsed = list(parsed)
+    parsed[0] = http_scheme
+    parsed[2] = http_path + '/'
+    searcher_url = urlparse.urlunparse(parsed)
+    search_module, search_klass = SEARCH_SCHEMES[search_scheme]
+    searcher = importlib.import_module(search_module)
+    searcher = getattr(searcher, search_klass)
+    return searcher(index_name, searcher_url)
+
+
 class AbstractSearch(object):
 
     def __init__(self, index):
