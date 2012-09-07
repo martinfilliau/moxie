c7e96ce9363db5996d0dd9e17322ae89b66d82ba
David King <dave@davbo.org>
2012-09-07 15:24:30 +0100

    Some default settings for the importers. Not sure where configuration should live. Or if we need multiple config files.

--- moxie/default_settings.py
+++ moxie/default_settings.py
@@ -1,2 +1,6 @@
 SEARCHER_URL = 'solr+http://localhost:8983/solr/collection1'
 KV_STORE_URL = 'redis://localhost:6379/0'
+
+OSM_IMPORT_URL = 'http://download.geofabrik.de/osm/europe/great_britain/england/oxfordshire.osm.bz2'
+OXPOINTS_IMPORT_URL = 'http://oxpoints.oucs.ox.ac.uk/all.json'
+NAPTAN_IMPORT_URL = 'http://www.dft.gov.uk/NaPTAN/snapshot/NaPTANxml.zip'
