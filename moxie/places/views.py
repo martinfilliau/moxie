8ad87232178a28bcf2ca817c6ed42a4af428a7c5
Dave King <dave@davbo.org>
2012-09-10 14:04:32 +0100

    Places views updated with ServiceView API Removes logic from the json response function into a common handle response func

--- moxie/places/views.py
+++ moxie/places/views.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify, render_template
-from moxie.core.views import ServiceView, register_mimetype
+from moxie.core.views import ServiceView, accepts
 from moxie.core.search import searcher
 
 
@@ -12,17 +12,19 @@ class Search(ServiceView):
     def get_results(self, query, location):
         return searcher.search_nearby(query, location)
 
-    @register_mimetype('application/json')
-    def json_results(self):
+    def handle_request(self):
         query = request.args.get('q', '*')
         location = request.args.get('lat', None), request.args.get('lon', None)
-        results = self.get_results(query, location)
-        # TODO should be extracted from this method, and new query should be shown to the user
-        if results.json['response']['numFound'] == 0:
-            new_query = str(results.json['spellcheck']['suggestions'][-1])
-            results = self.get_results(new_query, location)
-        return jsonify(results.json)
+        if location == (None, None):
+            return dict()
+        else:
+            results = self.get_results(query, location)
+            # TODO should be extracted from this method, and new query should be shown to the user
+            if results.json['response']['numFound'] == 0:
+                new_query = str(results.json['spellcheck']['suggestions'][-1])
+                results = self.get_results(new_query, location)
+            return results.json
 
-    @register_mimetype('text/html')
-    def html_form(self):
-        return render_template('search.html')
+    @accepts('text/html')
+    def as_html(self, response):
+        return render_template('search.html', **response)
