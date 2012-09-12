30b399b6d5c855d00ff5d39d4a473560530a0358
David King <dave@davbo.org>
2012-09-12 18:34:07 +0100

    js included from the correct template. Using the html5 data-* attr to store lat/lon

--- moxie/places/__init__.py
+++ moxie/places/__init__.py
@@ -2,7 +2,8 @@ from flask import Blueprint
 
 from .views import Search
 
-places = Blueprint('places', __name__, template_folder='templates')
+places = Blueprint('places', __name__, template_folder='templates',
+        static_folder='static')
 
 # URL Rules
 places.add_url_rule('/search', view_func=Search.as_view('search'))
