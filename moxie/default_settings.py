6a57f1d91a53ef2e987db68c22a7dd6f2dd3bb35
David King <dave@davbo.org>
2012-09-14 17:51:39 +0100

    Debug set in cfg file

--- moxie/default_settings.py
+++ moxie/default_settings.py
@@ -1,6 +1,11 @@
+# Moxie general config
+DEBUG = True
+
+# Moxie data API config
 SEARCHER_URL = 'solr+http://localhost:8983/solr/collection1'
 KV_STORE_URL = 'redis://localhost:6379/0'
 
+# Places app
 OSM_IMPORT_URL = 'http://download.geofabrik.de/osm/europe/great_britain/england/oxfordshire.osm.bz2'
 OXPOINTS_IMPORT_URL = 'http://oxpoints.oucs.ox.ac.uk/all.json'
 NAPTAN_IMPORT_URL = 'http://www.dft.gov.uk/NaPTAN/snapshot/NaPTANxml.zip'
--- runserver.py
+++ runserver.py
@@ -1,3 +1,3 @@
 from moxie import create_app
 app = create_app()
-app.run(debug=True)
+app.run()
