10a76a99f09c6264774f8c80442fbd666d23132f
David King <dave@davbo.org>
2012-09-14 13:56:32 +0100

    ServiceMetaclass responses now registered at class construction rather than instance init. Recommend reading the code / docstring. Thanks/blame to @inglesp

--- moxie/core/views.py
+++ moxie/core/views.py
@@ -16,30 +16,42 @@ def accepts(*accept_values):
     return wrapper
 
 
-class ServiceView(View):
-
-    default_response = NotAcceptable
-
-    def __init__(self, *args, **kwargs):
-        self.service_responses = defaultdict(list)
-        for attr in dir(self):
-            attr = getattr(self, attr)
+class ServiceMetaclass(type):
+    """Metaclass which picks up any Service Responses and registers them.
+    Service Responses:
+        Methods which are decorated with @accepts this sets a .mimetypes
+        attribute on the method listing any mimetypes which the method supports
+    Note: We also register service_responses from our base classes.
+    """
+    def __new__(cls, name, bases, attrs):
+        service_responses = dict()
+        for b in bases:  # Bases first so they get (correctly?) overriden
+            service_responses.update(getattr(b, 'service_responses', dict()))
+        for attr in attrs.values():
             mimetypes = getattr(attr, 'mimetypes', [])
             for mt in mimetypes:
-                self.service_responses[mt] = attr
-        super(ServiceView, self).__init__(*args, **kwargs)
+                service_responses[mt] = attr
+        attrs['service_responses'] = service_responses
+        return type.__new__(cls, name, bases, attrs)
+
+
+class ServiceView(View):
+
+    __metaclass__ = ServiceMetaclass
+    default_response = NotAcceptable
 
     def dispatch_request(self):
         """Finds the best_match service_response from those registered
         If no good service_response can be found we generally want to
         return a NotAcceptable 406.
         """
+        print self.service_responses
         best_match = request.accept_mimetypes.best_match(
                 self.service_responses.keys())
         if best_match:
             service_response = self.service_responses[best_match]
             response = self.handle_request()
-            return service_response(response)
+            return service_response(self, response)
         else:
             return self.default_response()
 
