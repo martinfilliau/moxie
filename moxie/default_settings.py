24108b0b7a8368b7baeb8e1337774c8fe23cfae2
David King <dave@davbo.org>
2012-09-19 10:25:49 +0100

    Default location

--- moxie/default_settings.py
+++ moxie/default_settings.py
@@ -6,6 +6,7 @@ SEARCHER_URL = 'solr+http://localhost:8983/solr/collection1'
 KV_STORE_URL = 'redis://localhost:6379/0'
 
 # Places app
+DEFAULT_LOCATION = (51.7531, -1.2584)
 OSM_IMPORT_URL = 'http://download.geofabrik.de/osm/europe/great_britain/england/oxfordshire.osm.bz2'
 OXPOINTS_IMPORT_URL = 'http://oxpoints.oucs.ox.ac.uk/all.json'
 NAPTAN_IMPORT_URL = 'http://www.dft.gov.uk/NaPTAN/snapshot/NaPTANxml.zip'
