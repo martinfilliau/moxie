a2a6285c8e36df6bc62f656596c4fcfcbaa9a5e7
Martin Filliau <martin@filliau.com>
2012-09-21 10:58:52 +0100

    Added basic handling of address/website to OxPoints importer.

--- moxie/places/importers/oxpoints.py
+++ moxie/places/importers/oxpoints.py
@@ -53,6 +53,19 @@ class OxpointsImporter(object):
 
             doc['identifiers'] = ids
 
+            """
+            Address properties:
+            "vCard_postal-code":"OX2 6JF",
+            "vCard_locality":"Oxford",
+            "vCard_extended-address":"St Antony's College",
+            "vCard_street-address":"62 Woodstock Road"},
+            """
+            address = "{0} {1}".format(self.tags.get("vCard_street-address", ""), self.tags.get("vCard_postal-code", ""))
+            result['address'] = " ".join(address.split())
+
+            if 'foaf_homepage' in datum:
+                result['website'] = datum['foaf_homepage']
+
             if 'geo_lat' in datum and 'geo_long' in datum:
                 doc['location'] = "%s,%s" % (datum.pop('geo_long'), datum.pop('geo_lat'))
             else:
@@ -67,14 +80,6 @@ class OxpointsImporter(object):
             result = prepare_document(doc, search_results.json, self.precedence)
             result = [result]
             self.indexer.index(result)
-
-
-
-            "vCard_postal-code":"OX2 6JF",
-"vCard_locality":"Oxford",
-"vCard_extended-address":"St Antony's College",
-"vCard_street-address":"62 Woodstock Road"},
-
         self.indexer.commit()
 
 
