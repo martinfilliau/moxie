a76a4a2e1729a31d1ef8fa9d92349bc2ac3d8336
Martin Filliau <martin@filliau.com>
2012-09-21 11:27:16 +0100

    Done a bit of refactoring on OxPoints importer.

--- moxie/places/importers/oxpoints.py
+++ moxie/places/importers/oxpoints.py
@@ -1,7 +1,10 @@
 import json
 import re
+import logging
 
 from moxie.places.importers.helpers import prepare_document
+
+logger = logging.getLogger(__name__)
 
 
 class OxpointsImporter(object):
@@ -24,63 +27,73 @@ class OxpointsImporter(object):
     def import_data(self):
         data = json.load(self.oxpoints_file)
         for datum in data:
-            name = datum.get('oxp_fullyQualifiedTitle', datum.get('dc_title', ''))
-            if not name:
-                continue
-            oxpoints_id = datum['uri'].rsplit('/')[-1]
-            if datum.get('type', '') == 'http://xmlns.com/foaf/0.1/Image' or oxpoints_id.endswith('.jpg'):
-                continue
-            doc = dict()
-            doc['name'] = name
-
-            oxpoints_type = datum.get('type', '').rsplit('#')[-1]
-            doc['tags'] = [oxpoints_type]
-            doc['type'] = "/university/{0}".format(oxpoints_type)
-
-            ids = list()
-            ids.append('oxpoints:{0}'.format(oxpoints_id))
-
-            for oxp_property, identifier in self.IDENTIFIERS.items():
-                if oxp_property in datum:
-                    value = datum.pop(oxp_property)
-                    if identifier == "osm":
-                        value = value.split('/')[1]
-                    if type(value) is list:
-                        for val in value:
-                            ids.append('{0}:{1}'.format(identifier, val.replace(' ', '-').replace('/', '-')))
-                    else:
-                        ids.append('{0}:{1}'.format(identifier, value.replace(' ', '-').replace('/', '-')))
-
-            doc['identifiers'] = ids
-
-            """
-            Address properties:
-            "vCard_postal-code":"OX2 6JF",
-            "vCard_locality":"Oxford",
-            "vCard_extended-address":"St Antony's College",
-            "vCard_street-address":"62 Woodstock Road"},
-            """
-            address = "{0} {1}".format(self.tags.get("vCard_street-address", ""), self.tags.get("vCard_postal-code", ""))
-            result['address'] = " ".join(address.split())
-
-            if 'foaf_homepage' in datum:
-                result['website'] = datum['foaf_homepage']
-
-            if 'geo_lat' in datum and 'geo_long' in datum:
-                doc['location'] = "%s,%s" % (datum.pop('geo_long'), datum.pop('geo_lat'))
-            else:
-                continue
-            doc.update([('raw_oxpoints_{0}'.format(k), v) for k, v in datum.items()])
-            for k, v in doc.items():
-                if type(v) not in [str, unicode] and k != self.identifier_key:
-                    doc.pop(k)
-
-            search_results = self.indexer.search_for_ids(
-                    self.identifier_key, doc[self.identifier_key])
-            result = prepare_document(doc, search_results.json, self.precedence)
-            result = [result]
-            self.indexer.index(result)
+            try:
+                self.process_datum(datum)
+            except Exception as e:
+                logger.warning("Couldn't process an item", e)
         self.indexer.commit()
+
+    def process_datum(self, datum):
+        """
+        Process a single OxPoint
+        @param datum: dict with item
+        """
+        name = datum.get('oxp_fullyQualifiedTitle', datum.get('dc_title', ''))
+        if not name:
+            pass
+        oxpoints_id = datum['uri'].rsplit('/')[-1]
+        if datum.get('type', '') == 'http://xmlns.com/foaf/0.1/Image' or oxpoints_id.endswith('.jpg'):
+            pass
+        doc = dict()
+        doc['name'] = name
+
+        oxpoints_type = datum.get('type', '').rsplit('#')[-1]
+        doc['tags'] = [oxpoints_type]
+        doc['type'] = "/university/{0}".format(oxpoints_type)
+
+        ids = list()
+        ids.append('oxpoints:{0}'.format(oxpoints_id))
+
+        for oxp_property, identifier in self.IDENTIFIERS.items():
+            if oxp_property in datum:
+                value = datum.pop(oxp_property)
+                if identifier == "osm":
+                    value = value.split('/')[1]
+                if type(value) is list:
+                    for val in value:
+                        ids.append('{0}:{1}'.format(identifier, val.replace(' ', '-').replace('/', '-')))
+                else:
+                    ids.append('{0}:{1}'.format(identifier, value.replace(' ', '-').replace('/', '-')))
+
+        doc['identifiers'] = ids
+
+        """
+        Address properties:
+        "vCard_postal-code":"OX2 6JF",
+        "vCard_locality":"Oxford",
+        "vCard_extended-address":"St Antony's College",
+        "vCard_street-address":"62 Woodstock Road"},
+        """
+        address = "{0} {1}".format(datum.get("vCard_street-address", ""), datum.get("vCard_postal-code", ""))
+        doc['address'] = " ".join(address.split())
+
+        if 'foaf_homepage' in datum:
+            doc['website'] = datum['foaf_homepage']
+
+        if 'geo_lat' in datum and 'geo_long' in datum:
+            doc['location'] = "%s,%s" % (datum.pop('geo_long'), datum.pop('geo_lat'))
+        else:
+            pass
+        doc.update([('raw_oxpoints_{0}'.format(k), v) for k, v in datum.items()])
+        for k, v in doc.items():
+            if type(v) not in [str, unicode] and k != self.identifier_key:
+                doc.pop(k)
+
+        search_results = self.indexer.search_for_ids(
+            self.identifier_key, doc[self.identifier_key])
+        result = prepare_document(doc, search_results.json, self.precedence)
+        result = [result]
+        self.indexer.index(result)
 
 
 def main():
