d0eabb5c9f7844dcbba3061396c9d186cca62ac6
Martin Filliau <martin@filliau.com>
2012-09-25 11:15:47 +0100

    Removed handling of static files from places.

--- moxie/places/__init__.py
+++ moxie/places/__init__.py
@@ -1,9 +1,5 @@
-from flask import Blueprint
-
 from .views import Search
 
-places = Blueprint('places', __name__, template_folder='templates',
-        static_folder='static')
 
 # URL Rules
 places.add_url_rule('/search', view_func=Search.as_view('search'))
