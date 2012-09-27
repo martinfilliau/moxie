c9a22d04bd474a80b2ddbfb670ad5f67afe7a93a
Martin Filliau <martin@filliau.com>
2012-09-27 10:46:12 +0100

    runserver has an option --profiler to... profile requests.

--- runserver.py
+++ runserver.py
@@ -1,4 +1,16 @@
+import argparse
+
 from moxie import create_app
 from werkzeug.contrib import profiler
-action_profile = profiler.make_action(create_app)
-action_profile('localhost', 5000)
+
+parser = argparse.ArgumentParser(description='Run the Flask application.')
+parser.add_argument('--profiler', dest='profiler', help='Run the Flask app with a profiler.', action='store_true')
+
+args = parser.parse_args()
+
+if args.profiler:
+    action_profile = profiler.make_action(create_app)
+    action_profile('localhost', 5000)
+else:
+    app = create_app()
+    app.run()
