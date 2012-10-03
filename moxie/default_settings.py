96c3ce1f0abe3d79ab96fc93f6ea16ffe9ffebf8
Martin Filliau <martin@filliau.com>
2012-10-03 17:28:25 +0100

    Updated path to OSM dump.

--- moxie/default_settings.py
+++ moxie/default_settings.py
@@ -7,7 +7,7 @@ KV_STORE_URL = 'redis://localhost:6379/0'
 
 # Places app
 DEFAULT_LOCATION = (51.7531, -1.2584)
-OSM_IMPORT_URL = 'http://download.geofabrik.de/osm/europe/great_britain/england/oxfordshire.osm.bz2'
+OSM_IMPORT_URL = 'http://download.geofabrik.de/openstreetmap/europe/great_britain/england/oxfordshire.osm.bz2'
 OXPOINTS_IMPORT_URL = 'http://oxpoints.oucs.ox.ac.uk/all.json'
 NAPTAN_IMPORT_URL = 'http://www.dft.gov.uk/NaPTAN/snapshot/NaPTANxml.zip'
 
