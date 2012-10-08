9ef9ca6fe4221c07c94244c752c61629f46e810d
David King <dave@davbo.org>
2012-10-08 17:51:42 +0100

    Moved the BusRTI view into here

--- moxie/places/views.py
+++ moxie/places/views.py
@@ -1,6 +1,9 @@
+import json
+
 from flask import request, current_app, url_for, abort, redirect, jsonify
 from moxie.core.views import ServiceView, accepts
 from moxie.core.search import searcher
+from moxie.core.kv import kv_store
 from moxie.places.importers.helpers import simplify_doc_for_render
 
 
@@ -61,3 +64,27 @@ class PoiDetail(ServiceView):
     @accepts('application/json')
     def as_json(self, response):
         return response
+
+
+class BusRti(ServiceView):
+    methods = ['GET', 'OPTIONS']
+
+    CACHE_KEY_FORMAT = '{0}_naptan_{1}'
+    CACHE_EXPIRE = 10   # seconds for cache to expire
+
+    def __init__(self, service):
+        self.service = service
+
+    def handle_request(self, id):
+        cache = kv_store.get(self.CACHE_KEY_FORMAT.format(__name__, id))
+        if cache:
+            return json.loads(cache)
+        else:
+            services, messages = self.service.get_rti(id)
+            response = {
+                'services': services,
+                'messages': messages
+            }
+            kv_store.setex(self.CACHE_KEY_FORMAT.format(__name__, id),
+                    self.CACHE_EXPIRE, json.dumps(response))
+            return response
