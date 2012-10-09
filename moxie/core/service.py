82b8322872c94e2a9f5b3e3b16ef46f535912e82
David King <dave@davbo.org>
2012-10-09 18:09:51 +0100

    classmethod on Service to create from request/app context

--- moxie/core/service.py
+++ moxie/core/service.py
@@ -1,9 +1,31 @@
+from flask import _app_ctx_stack, request
+
+
 class NoProviderFound(Exception):
     pass
 
+
 class Service(object):
-    def __init__(self, providers):
-        self.providers = providers
+
+    def __init__(self, providers=None):
+        self.providers = providers or []
+
+    @classmethod
+    def from_context(cls, name='', blueprint_name=''):
+        ctx = _app_ctx_stack.top
+        name = name or cls.__name__
+        if not blueprint_name and request:
+            blueprint_name = request.blueprint
+        service_key = '.'.join((blueprint_name, name))
+        if not hasattr(ctx, 'moxie_services'):
+            ctx.moxie_services = dict()
+        if service_key in ctx.moxie_services:
+            return ctx.moxie_services[service_key]
+        else:
+            args, kwargs = ctx.app.config['SERVICES'][blueprint_name][name]
+            service = cls(*args, **kwargs)
+            ctx.moxie_services[service_key] = service
+            return service
 
     def provider_exists(self, doc):
         for provider in self.providers:
