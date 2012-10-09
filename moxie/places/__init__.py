c7e5400d2ff51969a4715552358a53f6b47b8494
David King <dave@davbo.org>
2012-10-09 18:37:22 +0100

    No configuration required here

--- moxie/places/__init__.py
+++ moxie/places/__init__.py
@@ -1,18 +1,11 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
 
-from .views import Search, PoiDetail, BusRti
-
-from moxie.transport.service import TransportService
-from moxie.transport.providers.cloudamber import CloudAmberBusRtiProvider
+from .views import Search, PoiDetail, RTI
 
 places = Blueprint('places', __name__)
 
 
-rti_providers = [CloudAmberBusRtiProvider(current_app.config['BUS_RTI_URL'])]
-rti_service = TransportService(providers=rti_providers)
-
 # URL Rules
 places.add_url_rule('/search', view_func=Search.as_view('search'))
-places.add_url_rule('/<path:id>', view_func=PoiDetail.as_view('poidetail'))
-places.add_url_rule('/<path:id>/rti', view_func=BusRti.as_view('busrti',
-    service=rti_service))
+places.add_url_rule('/<path:ident>', view_func=PoiDetail.as_view('poidetail'))
+places.add_url_rule('/<path:ident>/rti', view_func=RTI.as_view('rti'))
