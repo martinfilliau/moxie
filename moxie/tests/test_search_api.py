e6fa6feb3caa3be3025d6c677223d2e83784eb99
David King <dave@davbo.org>
2012-10-15 17:33:28 +0100

    Fixed the tests!

--- moxie/tests/test_search_api.py
+++ moxie/tests/test_search_api.py
@@ -1,7 +1,7 @@
 import unittest
 import mock
 
-from moxie.core.search import _find_searcher, SEARCH_SCHEMES
+from moxie.core.search import SEARCH_SCHEMES, SearchService
 
 
 MockSearcher = mock.Mock()
@@ -13,10 +13,10 @@ class SearchAPITestCase(unittest.TestCase):
         SEARCH_SCHEMES['foobar'] = ('moxie.tests.test_search_api', 'MockSearcher')
 
     def test_find_searcher(self):
-        searcher = _find_searcher('foobar+http://localhost:1234/foo/bar/baz')
+        searcher = SearchService._get_backend('foobar+http://localhost:1234/foo/bar/baz')
         MockSearcher.assert_called_with('baz', 'http://localhost:1234/foo/bar/')
         self.assertEqual(MockSearcher(), searcher)
 
     def test_find_missing_searcher(self):
         with self.assertRaises(KeyError):
-            _find_searcher('fooboo+http://localhost:1234/foo/bar/baz')
+            SearchService._get_backend('fooboo+http://localhost:1234/foo/bar/baz')
