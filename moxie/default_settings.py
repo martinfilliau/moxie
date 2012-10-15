e53e8aafd135df9298902f3cc4fa4d5952e629ba
David King <dave@davbo.org>
2012-10-15 17:25:38 +0100

    Updated config to use [Search,KV]Service's. Removing global config.

--- moxie/default_settings.py
+++ moxie/default_settings.py
@@ -1,10 +1,6 @@
 # Moxie general config
 DEBUG = True
 
-# Moxie data API config
-SEARCHER_URL = 'solr+http://localhost:8983/solr/collection1'
-KV_STORE_URL = 'redis://localhost:6379/0'
-
 # Places app
 DEFAULT_LOCATION = (51.7531, -1.2584)
 OSM_IMPORT_URL = 'http://download.geofabrik.de/openstreetmap/europe/great_britain/england/oxfordshire.osm.bz2'
@@ -17,5 +13,7 @@ SERVICES = {
 'places': {
     'POIService': ([], {'providers': [bus_provider]}),
     'TransportService': ([], {'providers': [bus_provider]}),
+    'SearchService': (['solr+http://localhost:8983/solr/collection1'], {}),
+    'KVService': (['redis://localhost:6379/0'], {}),
     },
 }
