d9f4ca616b9b0f37687a562c6efa9c6df9203117
David King <dave@davbo.org>
2012-10-15 17:23:41 +0100

    Removed AbstractSearch. Added SearchService. Search can still be accessed by the same api's but should follow suit of the other Services and be created using the Application and Request contexts.

--- moxie/core/search/__init__.py
+++ moxie/core/search/__init__.py
@@ -1,7 +1,7 @@
 import urlparse
 import importlib
 
-from flask import _app_ctx_stack
+from moxie.core.service import Service
 from werkzeug.local import LocalProxy
 
 SEARCH_SCHEMES = {
@@ -9,57 +9,30 @@ SEARCH_SCHEMES = {
         }
 
 
-def get_searcher():
-    """Sets the searcher instance on the application context.
-    This is done for each application, so they can be configured with
-    different search indexes. Eg. Places on one collection, events in another.
+class SearchService(Service):
+    def __init__(self, backend_uri):
+        self.backend = self._get_backend(backend_uri)
 
-    The searcher is configured by setting a SEARCHER_URL in your config file.
-    """
-    ctx = _app_ctx_stack.top
-    searcher = getattr(ctx, 'moxie_searcher', None)
-    if searcher is None:
-        searcher_url = ctx.app.config['SEARCHER_URL']
-        searcher = _find_searcher(searcher_url)
-        ctx.moxie_searcher = searcher
-    return searcher
-searcher = LocalProxy(get_searcher)
-
-
-def _find_searcher(searcher_url):
-    """Parse the URL and imports the appropriate AbstractSearch implementation"""
-    parsed = urlparse.urlparse(searcher_url)
-    search_scheme, http_scheme = parsed.scheme.split('+')
-    http_path, _, index_name = parsed.path.rpartition('/')
-    parsed = list(parsed)
-    parsed[0] = http_scheme
-    parsed[2] = http_path + '/'
-    searcher_url = urlparse.urlunparse(parsed)
-    search_module, search_klass = SEARCH_SCHEMES[search_scheme]
-    searcher = importlib.import_module(search_module)
-    searcher = getattr(searcher, search_klass)
-    return searcher(index_name, searcher_url)
-
-
-class AbstractSearch(object):
-
-    def __init__(self, index):
+    def _get_backend(self, backend_uri):
+        """Parse the URI and imports the appropriate AbstractSearch
+        implementation
         """
-        @param index name of the index / collection / core
-        """
-        pass
+        parsed = urlparse.urlparse(backend_uri)
+        search_scheme, http_scheme = parsed.scheme.split('+')
+        http_path, _, index_name = parsed.path.rpartition('/')
+        parsed = list(parsed)
+        parsed[0] = http_scheme
+        parsed[2] = http_path + '/'
+        searcher_url = urlparse.urlunparse(parsed)
+        search_module, search_klass = SEARCH_SCHEMES[search_scheme]
+        searcher = importlib.import_module(search_module)
+        searcher = getattr(searcher, search_klass)
+        return searcher(index_name, searcher_url)
 
-    def index(self, document):
-        raise NotImplementedError
+    def search_nearby(self, query, location):
+        return self.backend.search_nearby(query, location)
 
-    def search(self, query):
-        raise NotImplementedError
+    def get_by_ids(self, ids):
+        return self.backend.get_by_ids(ids)
 
-    def clear_index(self):
-        raise NotImplementedError
-
-    def get_by_ids(self, document_ids):
-        """
-        Get documents by their unique ID
-        """
-        raise NotImplementedError
+searcher = LocalProxy(SearchService.from_context)
