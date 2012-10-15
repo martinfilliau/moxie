c150d4c86eb9f9d86ce65ab634cd2b529dcd0e8e
Martin Filliau <martin@filliau.com>
2012-10-15 17:48:10 +0100

    Updated fabfile for deploying moxie-js-client.

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,7 @@ from fabric.contrib.files import exists
 from fabric import utils
 
 MOXIE_REPO = "git://github.com/ox-it/moxie.git"
+MOXIE_CLIENT_REPO = "git://github.com/ox-it/moxie-js-client.git"
 
 """
 This script has to be used as follow:
@@ -30,8 +31,10 @@ def dev():
     env.environment = 'dev'
     env.hosts = ['33.33.33.10']
     env.user = 'moxie'
-    env.remote_install_dir = '/srv/moxie/precise32.oucs.ox.ac.uk'
-    env.remote_git_checkout = '/srv/moxie/source-moxie'
+    env.remote_install_dir_api = '/srv/moxie/precise32.oucs.ox.ac.uk'
+    env.remote_git_checkout_api = '/srv/moxie/source-moxie'
+    env.remote_install_dir_front = '/srv/moxie/js.mox'
+    env.remote_git_checkout_front = '/srv/moxie/source-moxie-js'
 
 
 @task
@@ -42,8 +45,10 @@ def staging():
     env.environment = 'staging'
     env.hosts = ['api.m.ox.ac.uk']
     env.user = 'moxie'
-    env.remote_install_dir = '/srv/moxie/api.m.ox.ac.uk'
-    env.remote_git_checkout = '/srv/moxie/source-moxie'
+    env.remote_install_dir_api = '/srv/moxie/api.m.ox.ac.uk'
+    env.remote_git_checkout_api = '/srv/moxie/moxie-api'
+    env.remote_install_dir_front = '/srv/moxie/new.m.ox.ac.uk'
+    env.remote_git_checkout_front = '/srv/moxie/moxie-js'
 
 
 """
@@ -52,7 +57,7 @@ Methods
 
 
 @task
-def deploy(version):
+def deploy_api(version):
     """
     Deploy mobileoxford (with given version - tag or branch) on defined
     environment in a virtual env
@@ -62,7 +67,7 @@ def deploy(version):
     if not version:
         utils.abort('You must specify a version (whether branch or tag).')
 
-    git_hash = git_branch(version)
+    git_hash = git_branch(env.remote_git_checkout_api, MOXIE_REPO, version)
 
     versioned_path = '/srv/%s/api-%s-%s' % (env.user, datetime.now().strftime('%Y%m%d%H%M')
             , git_hash)
@@ -70,12 +75,37 @@ def deploy(version):
     createvirtualenv(versioned_path)
     with prefix('source %s' % os.path.join(versioned_path, 'bin', 'activate')):
         install_moxie()
-        run('rm -f %s' % env.remote_install_dir)
-        run('ln -s %s %s' % (versioned_path, env.remote_install_dir))
+        run('rm -f %s' % env.remote_install_dir_api)
+        run('ln -s %s %s' % (versioned_path, env.remote_install_dir_api))
         run('circusctl stop moxie-celery')
         run('circusctl start moxie-celery')
         run('circusctl stop moxie-uwsgi')
         run('circusctl start moxie-uwsgi')
+
+
+@task
+def deploy_front(version):
+    """
+    Deploy the front-end in a versioned folder and does a symlink
+    """
+
+    require('user', provided_by=ENVIRONMENTS)
+
+    if not version:
+        utils.abort('You must specify a version (whether branch or tag).')
+
+    git_hash = git_branch(env.remote_git_checkout_front, MOXIE_CLIENT_REPO, version)
+
+    versioned_path = '/srv/%s/client-%s-%s' % (env.user, datetime.now().strftime('%Y%m%d%H%M')
+            , git_hash)
+            
+    run('mkdir {0}'.format(versioned_path))
+    run('cp -R {0}/* {1}'.format(env.remote_git_checkout_front, versioned_path))
+    with(cd(versioned_path)):
+        run('compass compile')
+        run('handlebars handlebars/places/ handlebars -f js/moxie.places.templates.js')
+    run('rm -f %s' % env.remote_install_dir_front)
+    run('ln -s %s %s' % (versioned_path, env.remote_install_dir_front))
 
 
 """
@@ -87,40 +117,19 @@ def createvirtualenv(path):
 
 
 def install_moxie():
-    require('remote_git_checkout', provided_by=ENVIRONMENTS)
-    with cd(env.remote_git_checkout):
+    require('remote_git_checkout_api', provided_by=ENVIRONMENTS)
+    with cd(env.remote_git_checkout_api):
         run('python setup.py install')
 
 
-def git_check_existing_repo():
-    """
-    Checks that git repo exists, create it if it doesn't
-    """
-    if not exists(env.remote_git_checkout):
-        run('git clone %s %s' % (MOXIE_REPO, env.remote_git_checkout))
-        with cd(env.remote_git_checkout):
-            run('git submodule init')
-
-
-def git_branch(name):
+def git_branch(git_checkout, git_repo, name):
     """
     Do a checkout on a branch
     """
-    git_check_existing_repo()
-    with cd(env.remote_git_checkout):
+    if not exists(git_checkout):
+        run('git clone %s %s' % (git_repo, git_checkout))
+
+    with cd(git_checkout):
         run('git fetch origin')
         run('git checkout origin/%s' % name)
-        run('git submodule update')
         return run('git rev-parse HEAD')
-
-
-def git_tag(name):
-    """
-    Do a checkout on a tag (or commit's hash)
-    """
-    git_check_existing_repo()
-    with cd(env.remote_git_checkout):
-        run('git fetch origin')
-        run('git checkout %s' % name)
-        run('git submodule update')
-        return name
