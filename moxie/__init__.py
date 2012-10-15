ae4ec2b8ef5e2cbb5b1dc5ee828867241eef682c
David King <dave@davbo.org>
2012-10-15 16:27:22 +0100

    Created blueprint factories

--- moxie/__init__.py
+++ moxie/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from moxie.places import places_blueprint
-from moxie.library import library_blueprint
+from moxie.places import create_blueprint as create_places_blueprint
+from moxie.library import create_blueprint as create_library_blueprint
 
 
 def create_app():
@@ -13,6 +13,8 @@ def create_app():
         pass
 
     # Register Moxie apps
-    app.register_blueprint(places_blueprint, url_prefix='/places')
-    app.register_blueprint(library_blueprint, url_prefix='/library')
+    app.register_blueprint(create_places_blueprint('places'),
+            url_prefix='/places')
+    app.register_blueprint(create_library_blueprint('library'),
+            url_prefix='/library')
     return app
--- moxie/places/__init__.py
+++ moxie/places/__init__.py
@@ -2,15 +2,18 @@ from flask import Blueprint
 
 from .views import Search, PoiDetail, RTI
 
-places_blueprint = Blueprint('places', __name__)
 
+def create_blueprint(blueprint_name):
+    places_blueprint = Blueprint(blueprint_name, __name__)
 
-# URL Rules
-places_blueprint.add_url_rule('/search',
-        view_func=Search.as_view('search'))
+    # URL Rules
+    places_blueprint.add_url_rule('/search',
+            view_func=Search.as_view('search'))
 
-places_blueprint.add_url_rule('/<path:ident>',
-        view_func=PoiDetail.as_view('poidetail'))
+    places_blueprint.add_url_rule('/<path:ident>',
+            view_func=PoiDetail.as_view('poidetail'))
 
-places_blueprint.add_url_rule('/<path:ident>/rti',
-        view_func=RTI.as_view('rti'))
+    places_blueprint.add_url_rule('/<path:ident>/rti',
+            view_func=RTI.as_view('rti'))
+
+    return places_blueprint
