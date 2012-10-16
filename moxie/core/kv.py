10226cbf8d823bc7fe3713f4975c6d60aa64857c
David King <dave@davbo.org>
2012-10-16 16:20:39 +0100

    KeyValueService follows the pattern set in SearchService Rather than import 'redis' directly it is referenced imported through importlib.

--- moxie/core/kv.py
+++ moxie/core/kv.py
@@ -1,38 +1,53 @@
 import urlparse
+import importlib
 
 from moxie.core.service import Service
 from werkzeug.local import LocalProxy
 
-SUPPORTED_KV_STORES = ('redis',)
+SUPPORTED_KV_STORES = {'redis': ('redis', 'StrictRedis')}
 
 
 class KVService(Service):
+    """:class:`~moxie.core.service.Service` for accessing a Key-Value store.
+    This is the secondary datastore used within Moxie. General usage should
+    be for caching and as a non-critical data store.
+
+    Most development has taken place with `redis <http://redis.io>`_ being used
+    as the KV store. Currently this is the only fully supported KV store, see:
+    :py:data:`moxie.core.kv.SUPPORTED_KV_STORES` for details.
+    """
 
     def __init__(self, backend_uri):
-        self.backend = self._get_backend(backend_uri)
+        self._backend = self._get_backend(backend_uri)
 
     @staticmethod
-    def _get_backend(kv_url):
-        kv_url = urlparse.urlparse(kv_url)
-        if kv_url.scheme not in SUPPORTED_KV_STORES:
-            raise NotImplemented("Moxie does not support %s." % kv_url.scheme)
-        if kv_url.scheme == 'redis':
-            import redis
-            host = kv_url.hostname
-            port = int(kv_url.port) if kv_url.port else 6379
-            db = kv_url.path.strip('/')
+    def _get_backend(kv_uri):
+        """Following the same pattern found in
+        :py:func:`moxie.core.search.SearchService._get_backend`
+
+        :param kv_uri: URI to the Key-value store for example
+                       ``redis://foo.bar/bucket``.
+        """
+        kv_uri = urlparse.urlparse(kv_uri)
+        if kv_uri.scheme in SUPPORTED_KV_STORES:
+            host = kv_uri.hostname
+            port = int(kv_uri.port) if kv_uri.port else 6379
+            db = kv_uri.path.strip('/')
             db = db or 0
-            kv = redis.StrictRedis(host=host, port=port, db=db,
-                    password=kv_url.password)
-        return kv
+            kv_module, kv_klass = SUPPORTED_KV_STORES[kv_uri.scheme]
+            kv_imp = importlib.import_module(kv_module)
+            kv = getattr(kv_imp, kv_klass)
+            return kv(host=host, port=port, db=db, password=kv_uri.password)
+        else:
+            raise NotImplementedError("Moxie could not find an implementation for %s." % kv_uri.scheme)
 
     def get(self, key):
-        return self.backend.get(key)
+        return self._backend.get(key)
 
     def set(self, key, value):
-        return self.backend.set(key, value)
+        return self._backend.set(key, value)
 
     def setex(self, key, expiry, value):
-        return self.backend.setex(key, expiry, value)
+        return self._backend.setex(key, expiry, value)
 
 kv_store = LocalProxy(KVService.from_context)
