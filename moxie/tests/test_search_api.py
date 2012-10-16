43db1976afa8d0eb66fba3722dbe383853201a1e
David King <dave@davbo.org>
2012-10-16 14:09:18 +0100

    Test creating SearchService

--- moxie/tests/test_search_api.py
+++ moxie/tests/test_search_api.py
@@ -20,3 +20,7 @@ class SearchAPITestCase(unittest.TestCase):
     def test_find_missing_searcher(self):
         with self.assertRaises(KeyError):
             SearchService._get_backend('fooboo+http://localhost:1234/foo/bar/baz')
+
+    def test_create_service(self):
+        ss = SearchService('foobar+http://localhost:1234/foo/bar/baz')
+        self.assertEqual(MockSearcher(), ss._backend)
