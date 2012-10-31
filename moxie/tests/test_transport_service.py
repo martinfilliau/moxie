42fb4153961e553b8e50c4d055848e6d9d0ef413
David King <dave@davbo.org>
2012-10-31 14:20:36 +0000

    Fixed tests, moved Transport Provider tests into their own TestCase

---
+++ moxie/tests/test_transport_service.py
@@ -0,0 +1,55 @@
+import unittest
+
+from moxie.transport.providers import TransportRTIProvider
+from moxie.transport.services import TransportService
+
+
+class ProvidesAll(TransportRTIProvider):
+    def handles(self, doc):
+        return True
+
+    def invoke(self, doc):
+        return doc
+
+
+class ProvidesNone(TransportRTIProvider):
+    def handles(self, doc):
+        return False
+
+    def invoke(self, doc):
+        return doc
+
+
+class TestProvider(TransportService):
+    def annotate_provider(self, doc):
+        if self.get_provider(doc):
+            doc['provider_exists'] = True
+            return doc
+        return doc
+
+
+class TransportServiceTest(unittest.TestCase):
+
+    def setUp(self):
+        self.any_service = TestProvider(providers=[ProvidesAll()])
+        self.no_service = TestProvider(providers=[ProvidesNone()])
+        self.doc = {'foo': 'bar'}
+
+    def test_get_provider(self):
+        self.assertTrue(self.any_service.get_provider(self.doc))
+
+    def test_none_get_provider(self):
+        self.assertEqual(None, self.no_service.get_provider(self.doc))
+
+    def test_annotated_doc(self):
+        doc = self.any_service.annotate_provider(self.doc)
+        self.assertTrue(doc['provider_exists'])
+
+    def test_unannotated_doc(self):
+        doc = self.no_service.annotate_provider(self.doc)
+        with self.assertRaises(KeyError):
+            doc['provider_exists']
+
+    def test_invoked_provider_success(self):
+        provider = self.any_service.get_provider(self.doc)
+        self.assertEqual(provider(self.doc), self.doc)
