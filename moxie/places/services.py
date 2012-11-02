522ba8dd3f26dfaa2d9d5c2085eaa90dec261861
Martin Filliau <martin@filliau.com>
2012-11-02 11:22:11 +0000

    Places / added method to search by identifiers.

--- moxie/places/services.py
+++ moxie/places/services.py
@@ -6,6 +6,11 @@ class POIService(Service):
     default_search = '*'
 
     def get_results(self, original_query, location):
+        """Search POIs
+        :param original_query: fts query
+        :param location: latitude,longitude
+        :return `py`class:SearchResponse` object
+        """
         query = original_query or self.default_search
         response = searcher.search_nearby(query, location)
         # if no results, try to use spellcheck suggestion to make a new request
@@ -16,6 +21,11 @@ class POIService(Service):
         return response
 
     def get_place_by_identifier(self, ident):
+        """Get a place by its main identifier
+        Search in all identifiers if not found.
+        :param ident: identifier to lookup
+        :return POI or None if no result
+        """
         response = searcher.get_by_ids([ident])
         # First do a GET request by its ID
         if response.results:
@@ -27,3 +37,14 @@ class POIService(Service):
                 return response.results[0]
             else:
                 return None
+
+    def search_place_by_identifier(self, ident):
+        """Search for a place by its identifiers
+        :param ident: identifier to lookup
+        :retur POI or None if no result
+        """
+        response = searcher.search_for_ids("identifiers", [ident])
+        if response.results:
+            return response.results[0]
+        else:
+            return None
