8cc7d9040bda938c7be12f5aed553d2ceb9aecfb
David King <dave@davbo.org>
2012-11-21 19:04:09 +0000

    Uses get_resource from core and the new blueprint_context

--- moxie/places/tasks.py
+++ moxie/places/tasks.py
@@ -1,117 +1,38 @@
-import requests
 import logging
-import urlparse
 import bz2
 import zipfile
 
-from tempfile import NamedTemporaryFile
 from xml.sax import make_parser
 
 from moxie import create_app
-from moxie.core.kv import KVService
-from moxie.core.search import SearchService
 from moxie.worker import celery
+from moxie.core.tasks import get_resource
+from moxie.core.search import searcher
 from moxie.places.importers.osm import OSMHandler
 from moxie.places.importers.oxpoints import OxpointsImporter
 from moxie.places.importers.naptan import NaPTANImporter
 
-app = create_app()
-with app.app_context():
-    kv_store = KVService.from_context(blueprint_name='places')
-    searcher = SearchService.from_context(blueprint_name='places')
 logger = logging.getLogger(__name__)
-ETAG_KEY_FORMAT = "%s_etag_%s"
-LOCATION_KEY_FORMAT = "%s_location_%s"
-
-
-def write_resource(response):
-    hostname = urlparse.urlparse(response.url).hostname
-    f = NamedTemporaryFile(prefix=app.import_name, suffix=hostname,
-            delete=False)
-    f.write(response.content)
-    location = f.name
-    f.close()
-    return location
-
-
-def get_cached_etag_location(url):
-    resource_etag_key = ETAG_KEY_FORMAT % (__name__, url)
-    resource_location_key = LOCATION_KEY_FORMAT % (__name__, url)
-    with app.app_context():
-        etag = kv_store.get(resource_etag_key)
-        location = kv_store.get(resource_location_key)
-        if location:
-            # Check to see if the cached file still exists
-            try:
-                f = open(location)
-                f.close()
-            except IOError:
-                etag, location = None, None
-    return etag, location
-
-
-def cache_etag_location(url, etag, location):
-    resource_etag_key = ETAG_KEY_FORMAT % (__name__, url)
-    resource_location_key = LOCATION_KEY_FORMAT % (__name__, url)
-    success = False
-    with app.app_context():
-        if etag and location:
-            kv_store.set(resource_etag_key, etag)
-            kv_store.set(resource_location_key, location)
-            success = True
-    return success
-
-
-def get_resource(url, force_update=False):
-    etag, location = get_cached_etag_location(url)
-    headers = {}
-    if etag and not force_update:
-        headers['if-none-match'] = etag
-    response = requests.get(url, headers=headers)
-    etag = response.headers.get('etag', None)
-    if response.status_code == 304:
-        # Unchanged
-        logger.info("ETag's match. No change resource - %s" % url)
-        return location
-    if response.ok:
-        logger.info("Downloaded - %s - Content-length: %s" % (
-            response.url, len(response.content)))
-        location = write_resource(response)
-        if cache_etag_location(url, etag, location):
-            logger.info("Successfully cached etag: %s for url: %s" % (etag, url))
-        return location
-    else:
-        logger.warning("Failed to download: %s Response: %s-%s" % (
-            url, response.status_code, response.reason))
-        return False
-
-
-@celery.task
-def clear_index():
-    with app.app_context():
-        r = searcher.clear_index()
-        logger.info(r)
+BLUEPRINT_NAME = 'places'
 
 
 @celery.task
 def import_all(force_update_all=False):
-    with app.app_context():
-        if force_update_all:
-            clear_index()
-        import_osm.delay(app.config['OSM_IMPORT_URL'],
-                force_update=force_update_all)
-        import_oxpoints.delay(app.config['OXPOINTS_IMPORT_URL'],
-                force_update=force_update_all)
-        import_naptan.delay(app.config['NAPTAN_IMPORT_URL'],
-                force_update=force_update_all)
+    app = create_app()
+    with app.blueprint_context(BLUEPRINT_NAME):
+        import_osm.delay(force_update=force_update_all)
+        import_oxpoints.delay(force_update=force_update_all)
+        import_naptan.delay(force_update=force_update_all)
 
 
 @celery.task
-def import_osm(url, force_update=False):
-    osm = get_resource(url, force_update)
-    logger.info("OSM Downloaded - Stored here: %s" % osm)
-    osm = open(osm)
-    with app.app_context():
+def import_osm(url=None, force_update=False):
+    app = create_app()
+    with app.blueprint_context(BLUEPRINT_NAME):
+        url = url or app.config['OSM_IMPORT_URL']
+        osm = get_resource(url, force_update)
+        logger.info("OSM Downloaded - Stored here: %s" % osm)
+        osm = open(osm)
         handler = OSMHandler(searcher, 5)
         parser = make_parser(['xml.sax.xmlreader.IncrementalParser'])
         parser.setContentHandler(handler)
@@ -125,20 +46,24 @@ def import_osm(url, force_update=False):
 
 
 @celery.task
-def import_oxpoints(url, force_update=False):
-    oxpoints = get_resource(url, force_update)
-    logger.info("OxPoints Downloaded - Stored here: %s" % oxpoints)
-    oxpoints = open(oxpoints)
-    with app.app_context():
+def import_oxpoints(url=None, force_update=False):
+    app = create_app()
+    with app.blueprint_context(BLUEPRINT_NAME):
+        url = url or app.config['OXPOINTS_IMPORT_URL']
+        oxpoints = get_resource(url, force_update)
+        logger.info("OxPoints Downloaded - Stored here: %s" % oxpoints)
+        oxpoints = open(oxpoints)
         importer = OxpointsImporter(searcher, 10, oxpoints)
         importer.import_data()
 
 
 @celery.task
-def import_naptan(url, force_update=False):
-    naptan = get_resource(url, force_update)
-    archive = zipfile.ZipFile(open(naptan))
-    f = archive.open('NaPTAN.xml')
-    with app.app_context():
-        naptan_importer = NaPTANImporter(searcher, 10, f, ['340'], 'identifiers')
-        naptan_importer.run()
+def import_naptan(url=None, force_update=False):
+    app = create_app()
+    with app.blueprint_context(BLUEPRINT_NAME):
+        url = url or app.config['NAPTAN_IMPORT_URL']
+        naptan = get_resource(url, force_update)
+        archive = zipfile.ZipFile(open(naptan))
+        f = archive.open('NaPTAN.xml')
+        naptan = NaPTANImporter(searcher, 10, f, ['340'], 'identifiers')
+        naptan.run()
