14419fe9d7f4a706fbf0e3b8bc59eb51708861df
David King <dave@davbo.org>
2012-11-26 17:43:50 +0000

    Added subclass of Celery to read config from YAML

--- moxie/worker.py
+++ moxie/worker.py
@@ -1,13 +1,39 @@
+import yaml
+import os
+
 from moxie import default_celeryconfig
 from celery import Celery
 
-celery = Celery(__name__)
 
+class MoxieCelery(Celery):
+
+    def config_from_yaml_envvar(self, envvar, silent=False):
+        """Reads a config in from `envvar` and parses as YAML and applies
+        this as the configuration.
+        """
+        yaml_path = os.environ.get(envvar)
+        if not yaml_path:
+            if silent:
+                return False
+            raise RuntimeError('The environment variable %r is not set '
+                               'and as such configuration could not be '
+                               'loaded.  Set this variable and make it '
+                               'point to a configuration file' %
+                               envvar)
+        try:
+            conf = yaml.safe_load(open(yaml_path))
+        except IOError as e:
+            if silent:
+                return False
+            e.strerror = 'Unable to load configuration file (%s)' % e.strerror
+            raise
+        self.config_from_object(conf)
+
+celery = MoxieCelery(__name__)
 # Default configuration shipped with moxie
 celery.config_from_object(default_celeryconfig)
-
 # Import user config using a env variable. Fails silently to use only defaults.
-celery.config_from_envvar('MOXIE_CELERYCONFIG', silent=True)
+celery.config_from_yaml_envvar('MOXIE_CELERYCONFIG', silent=True)
 
 if __name__ == '__main__':
     celery.start()
