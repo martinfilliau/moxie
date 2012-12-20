182af6bddfd527016a4d3c053e0f91f12a33d968
Martin Filliau <martin@filliau.com>
2012-12-20 12:39:49 +0100

    Refactoring healthchecks in core; returns 500 if no healthchecks configured
    Is it the correct behaviour?

--- moxie/core/healthchecks.py
+++ moxie/core/healthchecks.py
@@ -5,30 +5,39 @@ from flask import _app_ctx_stack, make_response
 
 logger = logging.getLogger(__name__)
 
-
-class Healthchecks(object):
-    
-    def __init__(self):
-        pass
         
 def check_services():
     ctx = _app_ctx_stack.top
     services = ctx.app.config.get('HEALTHCHECKS', [])
+    ok, result = run_healthchecks(services)
+    if ok:
+        response_code = 200
+    else:
+        response_code = 500
+    response = make_response('\n'.join(result), response_code)
+    response.headers['Content-Type'] = "text/plain"
+    return response
+
+
+def run_healthchecks(services):
+    """Run healthchecks by calling the method healthcheck() on every service.
+    :param services: list of services to check
+    :return True if everything is OK else False, string describing healthcheks"""
+    if not services:
+        return False, ["No healthchecks configured!"]
+    ok = True
     result = []
-    response_code = 200
     for service, parameters in services.iteritems():
         module_name, _, klass_name = service.rpartition('.')
         module = importlib.import_module(module_name)
         klass = getattr(module, klass_name)
         try:
-            ok, text = klass(**parameters).healthcheck()
+            success, text = klass(**parameters).healthcheck()
         except Exception as e:
-            ok = False
+            success = False
             text = e
-        if not ok:
-            response_code = 500
+        if not success:
+            ok = False
         result.append('* {service} [{parameters}]: {text}'.format(service=service,
             parameters=str(parameters), text=text))
-    response = make_response('\n'.join(result), response_code)
-    response.headers['Content-Type'] = "text/plain"
-    return response
+    return ok, result
