72262f0812dc6941f0434fd4bc09adc5401e8880
Martin Filliau <martin@filliau.com>
2012-12-20 12:10:09 +0100

    Basic (but working) healthcheck on configured services.
    Call a method "healthcheck" on every registered class in the configuration healthchecks. Accessing /_health by HTTP returns a list (in text/plain) of class names and statuses.

---
+++ moxie/core/healthchecks.py
@@ -0,0 +1,31 @@
+import importlib
+import logging
+
+from flask import _app_ctx_stack, make_response
+
+logger = logging.getLogger(__name__)
+
+
+class Healthchecks(object):
+    
+    def __init__(self):
+        pass
+        
+def check_services():
+    ctx = _app_ctx_stack.top
+    services = ctx.app.config.get('HEALTHCHECKS', [])
+    result = []
+    for service, parameters in services.iteritems():
+        module_name, _, klass_name = service.rpartition('.')
+        module = importlib.import_module(module_name)
+        klass = getattr(module, klass_name)
+        try:
+            ok, text = klass(**parameters).healthcheck()
+        except Exception as e:
+            ok = False
+            text = e
+        result.append('* {service} [{parameters}]: {text}'.format(service=service,
+            parameters=str(parameters), text=text))
+    response = make_response('\n'.join(result))
+    response.headers['Content-Type'] = "text/plain"
+    return response
