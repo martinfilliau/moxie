9ffbf207a5f536aa36064b410a479a99d3425a9c
Martin Filliau <martin@filliau.com>
2012-12-27 16:18:30 +0100

    Initial caching PoC using Flask-Cache, used in RTI view.

--- moxie/__init__.py
+++ moxie/__init__.py
@@ -1,5 +1,7 @@
 from os import path
+
 from moxie.core.configurator import Configurator
+from moxie.core.cache import cache
 from moxie.core.app import Moxie
 from moxie.core.healthchecks import check_services
 
@@ -10,5 +12,7 @@ def create_app():
     cfg_path = path.join(app.root_path, 'default_settings.yaml')
     configurator.from_yaml(cfg_path)
     configurator.from_envvar('MOXIE_SETTINGS', silent=True)
+    cache.init_app(app)
+    # Static URL Route for API Health checks
     app.add_url_rule('/_health', view_func=check_services)
     return app
