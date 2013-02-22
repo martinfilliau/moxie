73aa56a2ad89216a25b4a8baea1ec2b21870d06d
Martin Filliau <martin@filliau.com>
2013-02-22 14:36:44 +0000

    Initial configuration for Raven.

--- moxie/__init__.py
+++ moxie/__init__.py
@@ -1,6 +1,11 @@
 from os import path
 
 from werkzeug.exceptions import default_exceptions
+try:
+    from raven.contrib.flask import Sentry
+    raven_available = True
+except ImportError:
+    raven_available = False
 
 from moxie.core.configurator import Configurator
 from moxie.core.cache import cache
@@ -15,6 +20,8 @@ def create_app():
     cfg_path = path.join(app.root_path, 'default_settings.yaml')
     configurator.from_yaml(cfg_path)
     configurator.from_envvar('MOXIE_SETTINGS', silent=True)
+    if raven_available and 'SENTRY_DSN' in app.config:
+        sentry = Sentry(app)
     # Custom exceptions handler
     for code in default_exceptions.iterkeys():
         app.error_handler_spec[None][code] = exception_handler
