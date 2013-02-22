f44f3e58d2bdc4373dce574abe6277d213e45716
Martin Filliau <martin@filliau.com>
2013-02-22 14:45:49 +0000

    Added handler for logging.

--- moxie/__init__.py
+++ moxie/__init__.py
@@ -3,6 +3,8 @@ from os import path
 from werkzeug.exceptions import default_exceptions
 try:
     from raven.contrib.flask import Sentry
+    from raven.handlers.logging import SentryHandler
+    from raven.conf import setup_logging
     raven_available = True
 except ImportError:
     raven_available = False
@@ -20,10 +22,15 @@ def create_app():
     cfg_path = path.join(app.root_path, 'default_settings.yaml')
     configurator.from_yaml(cfg_path)
     configurator.from_envvar('MOXIE_SETTINGS', silent=True)
+
+    # logging configuration for Raven/Sentry
     if raven_available and 'SENTRY_DSN' in app.config:
         sentry = Sentry(dsn=app.config['SENTRY_DSN'])
         # capture uncaught exceptions within Flask
         sentry.init_app(app)
+        handler = SentryHandler(app.config['SENTRY_DSN'])
+        setup_logging(handler)
+
     # Custom exceptions handler
     for code in default_exceptions.iterkeys():
         app.error_handler_spec[None][code] = exception_handler
