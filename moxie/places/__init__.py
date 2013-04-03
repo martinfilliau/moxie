72e6c1c03ab64941f85cd93a3f77b80ce8079245
Martin Filliau <martin@filliau.com>
2013-04-03 19:02:10 +0100

    Initial attempt to provide links for places.

--- moxie/places/__init__.py
+++ moxie/places/__init__.py
@@ -1,11 +1,15 @@
 from flask import Blueprint
+from flask.helpers import url_for, make_response
 
 from moxie.transport.views import RTI
+from moxie.core.representations import HALRepresentation
 from .views import Search, PoiDetail, Types
 
 
 def create_blueprint(blueprint_name):
     places_blueprint = Blueprint(blueprint_name, __name__)
+
+    places_blueprint.add_url_rule('/', view_func=get_routes)
 
     places_blueprint.add_url_rule('/search',
             view_func=Search.as_view('search'))
@@ -20,3 +24,14 @@ def create_blueprint(blueprint_name):
             view_func=RTI.as_view('rti'))
 
     return places_blueprint
+
+
+def get_routes():
+    representation = HALRepresentation({})
+    representation.add_link('search', url_for('places.search'))
+    representation.add_link('types', url_for('places.types'))
+    representation.add_link('detail', '/places/{id}', templated=True)   # TODO cannot use url_for because templating
+    representation.add_link('rti', '/places/{id}/rti', templated=True)
+    response = make_response(representation.as_json(), 200)
+    response.headers['Content-Type'] = "application/json"
+    return response
