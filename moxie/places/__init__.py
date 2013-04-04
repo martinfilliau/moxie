f75d28b3379a9f3d281d28aa16d68e95b4c65132
Martin Filliau <martin@filliau.com>
2013-04-04 13:55:31 +0100

    Added more info on /places resource.

--- moxie/places/__init__.py
+++ moxie/places/__init__.py
@@ -30,10 +30,14 @@ def get_routes():
     path = 'places'     # TODO should be passed as argument?
     representation = HALRepresentation({})
     representation.add_curie('hl', 'http://moxie.readthedocs.org/en/latest/http_api/places.html#{rel}')
-    representation.add_link('hl:search', '/{bp}/search?q={{q}}'.format(bp=path), templated=True)
-    representation.add_link('hl:types', url_for('{bp}.types'.format(bp=path)))
-    representation.add_link('hl:detail', '/{bp}/{{id}}'.format(bp=path), templated=True)
-    representation.add_link('hl:rti', '/{bp}/{{id}}/rti'.format(bp=path), templated=True)
+    representation.add_link('self', '/{bp}'.format(bp=path))
+    representation.add_link('hl:search', '/{bp}/search?q={{q}}'.format(bp=path),
+                            templated=True, title='Search')
+    representation.add_link('hl:types', url_for('{bp}.types'.format(bp=path)), title='List of types')
+    representation.add_link('hl:detail', '/{bp}/{{id}}'.format(bp=path),
+                            templated=True, title='POI detail')
+    representation.add_link('hl:rti', '/{bp}/{{id}}/rti'.format(bp=path),
+                            templated=True, title='POI Real-Time Information')
     response = make_response(representation.as_json(), 200)
     response.headers['Content-Type'] = "application/json"
     return response
