e91bd5f51a2991df1eb5ea4a3d28a9388574acfb
Martin Filliau <martin@filliau.com>
2013-04-10 11:54:39 +0100

    Places / better handling of errors in tasks (will not try to process file if it has not been downloaded).

--- moxie/places/tasks.py
+++ moxie/places/tasks.py
@@ -31,18 +31,21 @@ def import_osm(url=None, force_update=False):
     with app.blueprint_context(BLUEPRINT_NAME):
         url = url or app.config['OSM_IMPORT_URL']
         osm = get_resource(url, force_update)
-        logger.info("OSM Downloaded - Stored here: %s" % osm)
-        osm = open(osm)
-        handler = OSMHandler(searcher, 5)
-        parser = make_parser(['xml.sax.xmlreader.IncrementalParser'])
-        parser.setContentHandler(handler)
-        # Parse in 8k chunks
-        buffer = osm.read(8192)
-        bunzip = bz2.BZ2Decompressor()
-        while buffer:
-            parser.feed(bunzip.decompress(buffer))
+        if osm:
+            logger.info("OSM Downloaded - Stored here: %s" % osm)
+            osm = open(osm)
+            handler = OSMHandler(searcher, 5)
+            parser = make_parser(['xml.sax.xmlreader.IncrementalParser'])
+            parser.setContentHandler(handler)
+            # Parse in 8k chunks
             buffer = osm.read(8192)
-        parser.close()
+            bunzip = bz2.BZ2Decompressor()
+            while buffer:
+                parser.feed(bunzip.decompress(buffer))
+                buffer = osm.read(8192)
+            parser.close()
+        else:
+            logger.info("OSM hasn't been imported - resource not loaded")
 
 
 @celery.task
@@ -51,10 +54,13 @@ def import_oxpoints(url=None, force_update=False):
     with app.blueprint_context(BLUEPRINT_NAME):
         url = url or app.config['OXPOINTS_IMPORT_URL']
         oxpoints = get_resource(url, force_update)
-        logger.info("OxPoints Downloaded - Stored here: %s" % oxpoints)
-        oxpoints = open(oxpoints)
-        importer = OxpointsImporter(searcher, 10, oxpoints)
-        importer.import_data()
+        if oxpoints:
+            logger.info("OxPoints Downloaded - Stored here: %s" % oxpoints)
+            oxpoints = open(oxpoints)
+            importer = OxpointsImporter(searcher, 10, oxpoints)
+            importer.import_data()
+        else:
+            logger.info("OxPoints hasn't been imported - resource not loaded")
 
 
 @celery.task
@@ -63,7 +69,10 @@ def import_naptan(url=None, force_update=False):
     with app.blueprint_context(BLUEPRINT_NAME):
         url = url or app.config['NAPTAN_IMPORT_URL']
         naptan = get_resource(url, force_update)
-        archive = zipfile.ZipFile(open(naptan))
-        f = archive.open('NaPTAN.xml')
-        naptan = NaPTANImporter(searcher, 10, f, ['340'], 'identifiers')
-        naptan.run()
+        if naptan:
+            archive = zipfile.ZipFile(open(naptan))
+            f = archive.open('NaPTAN.xml')
+            naptan = NaPTANImporter(searcher, 10, f, ['340'], 'identifiers')
+            naptan.run()
+        else:
+            logger.info("Naptan hasn't been imported - resource not loaded")
