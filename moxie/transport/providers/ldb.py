a3ee60c1c256d8f443d0cc49a590fa7dc990775f
David King <dave@davbo.org>
2013-05-31 16:48:49 +0100

    LDB provider implements RTI provider interface

--- moxie/transport/providers/ldb.py
+++ moxie/transport/providers/ldb.py
@@ -2,34 +2,60 @@ import logging
 import suds
 
 from suds.sax.element import Element
+from . import TransportRTIProvider
 
 
 logger = logging.getLogger(__name__)
 
 
-class LiveDepartureBoardPlacesProvider():
+class LiveDepartureBoardPlacesProvider(TransportRTIProvider):
     _WSDL_URL = "https://realtime.nationalrail.co.uk/ldbws/wsdl.aspx"
     _ATTRIBUTION = {'title': ("Powered by National Rail Enquiries"),
                   'url': "http://www.nationalrail.co.uk"}
 
-    def __init__(self, token, max_services=15):
+    def __init__(self, token, ldb_service=None, max_services=15):
+        self._token = token
         self._max_services = max_services
-        soapheaders = Element('AccessToken').insert(
-                Element('TokenValue').setText(token))
-        try:
-            self.ldb = suds.client.Client(self._WSDL_URL,
-                    soapheaders=soapheaders)
-        except:
-            logger.warning("Could not instantiate suds client for live departure board.",
-                    exc_info=True, extra={'wsdl_url': self._WSDL_URL})
+        self.ldb_service = None
+
+    def handles(self, doc):
+        for ident in doc.identifiers:
+            if ident.startswith('crs'):
+                return True
+        return False
+
+    def invoke(self, doc):
+        if not self.ldb_service:
+            try:
+                soapheaders = Element('AccessToken').insert(
+                        Element('TokenValue').setText(self._token))
+                self.ldb_service = suds.client.Client(self._WSDL_URL,
+                        soapheaders=soapheaders)
+            except:
+                logger.warning("Could not instantiate suds client for live departure board.",
+                        exc_info=True, extra={'wsdl_url': self._WSDL_URL})
+        for ident in doc.identifiers:
+            if ident.startswith('crs'):
+                _, crs_code = ident.split(':')
+                return self.get_departure_board(crs_code)
 
     def get_departure_board(self, crs):
-        db = self.ldb.service.GetDepartureBoard(self._max_services, crs)
-        return self.transform_suds(db)
+        db = self.ldb_service.service.GetDepartureBoard(self._max_services, crs)
+        db = self.transform_suds(db)
+        if 'nrccMessages' in db:
+            messages = db['nrccMessages']['message']
+        else:
+            messages = []
+        return db['trainServices']['service'], messages
 
     def get_arrival_board(self, crs):
-        db = self.ldb.service.GetArrivalBoard(self._max_services, crs)
-        return self.transform_suds(db)
+        db = self.ldb_service.service.GetArrivalBoard(self._max_services, crs)
+        db = self.transform_suds(db)
+        if 'nrccMessages' in db:
+            messages = db['nrccMessages']['message']
+        else:
+            messages = []
+        return db['trainServices']['service'], messages
 
     def transform_suds(self, o):
         if isinstance(o, suds.sudsobject.Object):
