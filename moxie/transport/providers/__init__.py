9fbb6c48f88ad8caf4e73cba1d9b519d26337700
David King <dave@davbo.org>
2013-06-21 17:49:01 +0100

    Updated providers to handle the new RTI links

--- moxie/transport/providers/__init__.py
+++ moxie/transport/providers/__init__.py
@@ -1,10 +1,12 @@
 class TransportRTIProvider(object):
 
+    provides = []
+
     def handles(self, doc):
         return False
 
     def invoke(self, doc):
         raise NotImplementedError("You must implement the 'invoke' method")
 
-    def __call__(self, doc):
-        return self.invoke(doc)
+    def __call__(self, *args, **kwargs):
+        return self.invoke(*args, **kwargs)
