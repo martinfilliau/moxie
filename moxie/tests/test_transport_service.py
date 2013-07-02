20be26c122aa5acc5211888319cc51126c10ffdc
David King <dave@davbo.org>
2013-07-02 17:17:08 +0100

    Updated tests for the new provider exceptions

--- moxie/tests/test_transport_service.py
+++ moxie/tests/test_transport_service.py
@@ -2,14 +2,19 @@ import unittest
 
 from moxie.transport.providers import TransportRTIProvider
 from moxie.transport.services import TransportService
+from moxie.core.service import NoSuitableProviderFound, MultipleProvidersFound
 
 
 class ProvidesAll(TransportRTIProvider):
     def handles(self, doc):
         return True
 
-    def invoke(self, doc):
-        return doc
+    def invoke(self, *args):
+        return args
+
+
+class ProvidesAllTwo(ProvidesAll):
+    pass
 
 
 class ProvidesNone(TransportRTIProvider):
@@ -33,25 +38,33 @@ class TransportServiceTest(unittest.TestCase):
     def setUp(self):
         provides_all_conf = {'moxie.tests.test_transport_service.ProvidesAll': {}}
         provides_none_conf = {'moxie.tests.test_transport_service.ProvidesNone': {}}
+        provides_all_twice_conf = {'moxie.tests.test_transport_service.ProvidesAll': {},
+                'moxie.tests.test_transport_service.ProvidesAllTwo': {}}
         self.any_service = TestProvider(providers=provides_all_conf)
         self.no_service = TestProvider(providers=provides_none_conf)
+        self.many_providers = TestProvider(providers=provides_all_twice_conf)
         self.doc = {'foo': 'bar'}
 
     def test_get_provider(self):
         self.assertTrue(self.any_service.get_provider(self.doc))
 
     def test_none_get_provider(self):
-        self.assertEqual(None, self.no_service.get_provider(self.doc))
+        with self.assertRaises(NoSuitableProviderFound):
+            self.no_service.get_provider(self.doc)
+
+    def test_multiple_providers(self):
+        with self.assertRaises(MultipleProvidersFound):
+            self.many_providers.get_provider(self.doc)
 
     def test_annotated_doc(self):
         doc = self.any_service.annotate_provider(self.doc)
         self.assertTrue(doc['provider_exists'])
 
-    def test_unannotated_doc(self):
-        doc = self.no_service.annotate_provider(self.doc)
-        with self.assertRaises(KeyError):
-            doc['provider_exists']
-
     def test_invoked_provider_success(self):
         provider = self.any_service.get_provider(self.doc)
-        self.assertEqual(provider(self.doc), self.doc)
+        self.assertEqual(provider(self.doc)[0], self.doc)
+
+    def test_invoked_provider_args_proxy(self):
+        provider = self.any_service.get_provider(self.doc)
+        args = (self.doc, 1, 2, 3, {1: True})
+        self.assertEqual(provider(*args), args)
