martinfilliau/moxie

commit a11a08073ff970a95a53cbdd204fb68451b7b0d8

Cloudamber provider: corrected representation when no services are available
Should always be a list

moxie/transport/providers/cloudamber.py
@@ -78,9 +78,9 @@ class CloudAmberBusRtiProvider(TransportRTIProvider):
         """
         Parse HTML content from a CloudAmber page
         @param content: HTML content
-        @return: dictionary of services, messages
+        @return: list of services, messages
         """
-        services = {}
+        services = []
         messages = []
         try:
             xml = etree.fromstring(content, parser=etree.HTMLParser())
@@ -90,6 +90,8 @@ class CloudAmberBusRtiProvider(TransportRTIProvider):
             # retrieved all cells, splitting every CELLS_PER_ROW to get rows
             CELLS_PER_ROW = 5
             rows = [cells[i:i+CELLS_PER_ROW] for i in range(0, len(cells), CELLS_PER_ROW)]
+
+            parsed_services = {}
 
             for row in rows:
                 service, destination, proximity = [row[i].text.encode('utf8').replace('\xc2\xa0', '')
@@ -101,12 +103,12 @@ class CloudAmberBusRtiProvider(TransportRTIProvider):
 
                 if not service in services:
                     # first departure of this service
-                    services[service] = (destination, (proximity, diff), [])
+                    parsed_services[service] = (destination, (proximity, diff), [])
                 else:
                     # following departure of this service
-                    services[service][2].append((proximity, diff))
+                    parsed_services[service][2].append((proximity, diff))
 
-            services = [(s[0], s[1][0], s[1][1], s[1][2]) for s in services.items()]
+            services = [(s[0], s[1][0], s[1][1], s[1][2]) for s in parsed_services.items()]
             services.sort(key = lambda x: ( ' '*(5-len(x[0]) + (1 if x[0][-1].isalpha() else 0)) + x[0] ))
             services.sort(key = lambda x: x[2][1])
 

commit 359c307efac65c046ebc89bca4afcd4be9aeb826

Fixed bug introduced in a11a080

moxie/transport/providers/cloudamber.py
@@ -101,7 +101,7 @@ class CloudAmberBusRtiProvider(TransportRTIProvider):
                 else:
                     diff = int(proximity.split(' ')[0])
 
-                if not service in services:
+                if not service in parsed_services:
                     # first departure of this service
                     parsed_services[service] = (destination, (proximity, diff), [])
                 else: