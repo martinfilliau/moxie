3ca67136cf095f5e0f49f9d6a805a455865c287d
Martin Filliau <martin@filliau.com>
2013-07-26 14:38:46 +0100

    Changed representation, added indexer

--- moxie/places/importers/ox_library_data.py
+++ moxie/places/importers/ox_library_data.py
@@ -32,9 +32,13 @@ def policies(l, n):
     pol = {}
     if e is not None:
         for policy in e:
-            pol[policy.get('for')] = {'access': POLICIES[policy.find('access').text],
-                                      'borrowing': POLICIES[policy.find('borrowing').text],
-                                      'notes': string_cleanup(policy.find('notes').text)}
+            #pol[policy.get('for')] = {'access': POLICIES[policy.find('access').text],
+            #                          'borrowing': POLICIES[policy.find('borrowing').text],
+            #                          'notes': string_cleanup(policy.find('notes').text)}
+            pol[policy.get('for')] = "Admissions: {access}. Borrowings: {borrowing}. Notes: {notes}".format(
+                access=POLICIES[policy.find('access').text],
+                borrowing=POLICIES[policy.find('borrowing').text],
+                notes=string_cleanup(policy.find('notes').text))
     return pol
 
 
@@ -47,22 +51,53 @@ def string_cleanup(str):
 
 class OxLibraryDataImporter(object):
 
-    def __init__(self, indexer, precedence, file):
+    def __init__(self, indexer, precedence, file, identifier_key='identifiers',
+                 lib_data_identifier='librarydata',
+                 prefix_index_key='meta_library_'):
         self.indexer = indexer
         self.precedence = precedence
         self.file = file
+        self.identifier_key = identifier_key
+        self.lib_data_identifier = lib_data_identifier
+        self.prefix_index_key = prefix_index_key
 
     def run(self):
+        self.parse_libs()
+        if self.indexer:
+            self.index()
+
+    def parse_libs(self):
         xml = etree.parse(self.file)
         libraries = xml.xpath('.//library')
-        self.libs = [{'name': text(l, 'name'),
-                      'id': text(l, 'id'),
-                      'opening_hours_term_time': text(l, 'hours/termtime'),
+        self.libs = [{'id': text(l, 'id'),
+                      'opening_hours_termtime': text(l, 'hours/termtime'),
                       'opening_hours_vacation': text(l, 'hours/vacation'),
                       'opening_hours_closed': text(l, 'hours/closed'),
                       'subjects': list(l, 'subjects/subject'),
                       'policies': policies(l, 'policies/policy')
                      } for l in libraries]
+
+    def index(self):
+        for lib in self.libs:
+            search_results = self.indexer.search_for_ids(self.identifier_key,
+                                                         "{key}:{value}".format(
+                                                             key=self.lib_data_identifier,
+                                                             value=lib['id']))
+            doc = search_results.results[0]
+            doc[self.prefix_index_key+'opening_hours_termtime'] = lib['opening_hours_termtime']
+            doc[self.prefix_index_key+'opening_hours_vacation'] = lib['opening_hours_vacation']
+            doc[self.prefix_index_key+'opening_hours_closed'] = lib['opening_hours_closed']
+            doc[self.prefix_index_key+'subject'] = lib['subjects']
+            if 'academic' in lib['policies']:
+                doc[self.prefix_index_key+'policy_academic'] = lib['policies']['academic']
+            if 'other' in lib['policies']:
+                doc[self.prefix_index_key+'policy_other'] = lib['policies']['other']
+            if 'postgraduate' in lib['policies']:
+                doc[self.prefix_index_key+'policy_postgraduate'] = lib['policies']['postgraduate']
+            if 'undergraduate' in lib['policies']:
+                doc[self.prefix_index_key+'policy_undergraduate'] = lib['policies']['undergraduate']
+            result = prepare_document(doc, search_results, self.precedence)
+            self.indexer.index([result])
 
 
 def main():
