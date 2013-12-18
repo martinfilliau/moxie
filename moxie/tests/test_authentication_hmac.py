81d5ee2950b2268e182173c405f3b66e775d236b
David King <dave@davbo.org>
2013-12-18 15:54:58 +0000

    Tests for HMAC authentication / failure states

--- moxie/tests/test_authentication_hmac.py
+++ moxie/tests/test_authentication_hmac.py
@@ -13,8 +13,8 @@ class DummyUser(object):
 class TestAuthenticatedView(HMACView):
 
     def handle_request(self):
-        print self._check_auth(DummyUser('mysupersecretkey'))
-        return {'name': 'Dave'}
+        if self._check_auth(DummyUser('mysupersecretkey')):
+            return {'name': 'Dave'}
 
     @accepts('foo/bar')
     def basic_response(self, response):
@@ -28,6 +28,34 @@ class HMACAuthenticationTestCase(unittest.TestCase):
         self.app.add_url_rule('/test', 'test', TestAuthenticatedView.as_view('test'))
 
     def test_successful_hmac(self):
+        headers = [
+            ('Accept', 'foo/bar'),
+            ('Date', 'Wednesday'),
+            ('X-HMAC-Nonce', 'foobarbaz'),
+            ('Authorization', '668db85d1dff6718d778454fc8c1d368a906f675'),
+        ]
         with self.app.test_client() as c:
-            rv = c.get('/foo', headers=[('Accept', 'foo/bar')])
-            print rv
+            rv = c.get('/test', headers=headers)
+            self.assertEqual(rv.status_code, 200)
+
+    def test_hmac_signature_mismatch(self):
+        headers = [
+            ('Accept', 'foo/bar'),
+            ('Date', 'Wednesday'),
+            ('X-HMAC-Nonce', 'foobarbaz'),
+            ('Authorization', 'wrong-wrong'),
+        ]
+        with self.app.test_client() as c:
+            rv = c.get('/test', headers=headers)
+            self.assertEqual(rv.status_code, 401)
+
+    def test_missing_header(self):
+        headers = [
+            ('Accept', 'foo/bar'),
+            ('Date', 'Wednesday'),
+            ('Authorization', 'wrong-wrong'),
+        ]
+        with self.app.test_client() as c:
+            rv = c.get('/test', headers=headers)
+            self.assertEqual(rv.status_code, 401)
+            self.assertIn("missing header", rv.headers['WWW-Authenticate'])
