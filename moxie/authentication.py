a0110e7ad34ba32ad34800a39ac979219fc685cf
David King <dave@davbo.org>
2013-12-18 15:54:41 +0000

    Updated HMAC with working implementation

--- moxie/authentication.py
+++ moxie/authentication.py
@@ -2,25 +2,56 @@ import hmac
 
 from hashlib import sha1
 from flask import request
+
 from moxie.core.views import ServiceView
+from moxie.core.exceptions import ApplicationException
+
+
+class HMACSignatureMismatch(ApplicationException):
+    status_code = 401
+    message = "Unauthorized"
+    digest_header = 'HMACDigest realm="{realm}", '
+    digest_header += 'reason="{reason}", '
+    digest_header += 'algorithm="{algorithm}", '
+
+    def __init__(self, realm="HMACDigest Moxie", reason="unauthorized",
+                 algorithm="HMAC-SHA-1", payload=None):
+        self.payload = payload
+        self.headers = {
+            'WWW-Authenticate': self.digest_header.format(
+                realm=realm, reason=reason, algorithm=algorithm
+            ),
+        }
 
 
 class HMACView(ServiceView):
-    _HMAC_HEADERS = ['date']
-    _HMAC_TEMPLATE = "{url}{data}{headers}"
+    _HMAC_HEADERS = ['date', 'x-hmac-nonce']
+    _HMAC_TEMPLATE = """{method}\n{url}\n{headers}"""
 
-    def _check_auth(self, user):
-        headers = [request.headers[header] for header in self._HMAC_HEADERS]
-        headers = ''.join(headers)
+    def _get_canonical_rep(self):
+        headers = []
+        try:
+            for header in self._HMAC_HEADERS:
+                headers.append("%s:%s" % (
+                    header.lower(), request.headers[header])
+                )
+        except KeyError as e:
+            raise HMACSignatureMismatch(
+                reason="missing header: %s" % e.message)
+        headers = '\n'.join(headers)
         context = {
+            'method': request.method.upper(),
             'url': request.url,
-            'data': request.get_data(),
             'headers': headers,
         }
-        message = self._HMAC_TEMPLATE.format(**context)
-        print message
-        hmac_hash = hmac.new(user.secret_key, message, sha1)
-        print hmac_hash
+        representation = self._HMAC_TEMPLATE.format(**context)
+        return representation
+
+    def _check_auth(self, user):
+        rep = self._get_canonical_rep()
+        hmac_hash = hmac.new(user.secret_key, rep, sha1)
         request_hash = request.headers['Authorization']
-        print request_hash
-        return request_hash == hmac_hash.hexdigest()
+        if request_hash == hmac_hash.hexdigest():
+            return True
+        else:
+            raise HMACSignatureMismatch()
