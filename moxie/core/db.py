9acf12fe4b6afd63cf06f83e80eaaaab151a500b
Martin Filliau <martin@filliau.com>
2014-01-16 11:55:26 +0000

    First draft of a DB healthcheck

--- moxie/core/db.py
+++ moxie/core/db.py
@@ -2,3 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+
+class DBHealthcheck(object):
+
+    def healthcheck(self):
+        try:
+            connection = db.engine.connect()
+            result = connection.execute("SELECT 1;")
+            result.first()
+            connection.close()
+            return True, 'OK'
+        except Exception as e:
+            return False, e
