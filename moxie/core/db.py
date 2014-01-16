c1ec5e82e47977875ad15491e427ccfaaff0e8c6
Martin Filliau <martin@filliau.com>
2014-01-16 12:03:47 +0000

    Added logging and comments

--- moxie/core/db.py
+++ moxie/core/db.py
@@ -1,4 +1,8 @@
+import logging
+
 from flask_sqlalchemy import SQLAlchemy
+
+logger = logging.getLogger(__name__)
 
 
 db = SQLAlchemy()
@@ -9,9 +13,12 @@ class DBHealthcheck(object):
     def healthcheck(self):
         try:
             connection = db.engine.connect()
+            # doing a simple SQL query "SELECT 1" on the configured database
             result = connection.execute("SELECT 1;")
+            # attempting to retrieve the first line of the result... or None
             result.first()
             connection.close()
             return True, 'OK'
         except Exception as e:
+            logger.error('Error while checking health of DB', exc_info=True)
             return False, e
