561a77291b9dbc7ab1885d7084c13d05c8ca9a6b
David King <dave@davbo.org>
2014-04-02 16:07:46 +0100

    Tests for qstring argument transforms

--- moxie/tests/test_places_services.py
+++ moxie/tests/test_places_services.py
@@ -20,3 +20,23 @@ class POIServiceTestCase(unittest.TestCase):
                 mock_doc_to_poi.return_value = None
                 poi_service.get_places_by_identifiers(['123', '456'])
                 mock_searcher.get_by_ids.assert_called_with(['123', '456'])
+
+    def test_transform_args_internal(self):
+        POIService.key_transforms = [('foo', 'bar')]
+        poi_service = POIService()
+        before = ['foobar']
+        after = poi_service._args_to_internal(before)
+        self.assertEqual(after, ['barbar'])
+
+    def test_transform_args_friendly(self):
+        POIService.key_transforms = [('foo', 'bar')]
+        poi_service = POIService()
+        before = ['barbar']
+        after = poi_service._args_to_friendly(before)
+        self.assertEqual(after, ['foobar'])
+
+    def test_transform_args_reversibile(self):
+        POIService.key_transforms = [('foo', 'bar')]
+        poi_service = POIService()
+        args = ['foobar', 'foofoo']
+        self.assertEqual(args, poi_service._args_to_friendly(poi_service._args_to_internal(args)))
