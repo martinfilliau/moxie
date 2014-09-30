5f80bdc2050e196fa1099f97f8d6df52b4ebd8ad
Martin Filliau <martin@filliau.com>
2014-09-30 12:15:50 +0100

    Simplify method

--- moxie/places/importers/oxpoints_helpers.py
+++ moxie/places/importers/oxpoints_helpers.py
@@ -12,20 +12,18 @@ def find_location(graph, subject, depth=1, max_depth=10):
     :return tuple (lat, lon) or None
     """
     if depth <= max_depth:
+        depth += 1
         if (subject, Geo.lat, None) in graph and (subject, Geo.long, None) in graph:
             lat = graph.value(subject, Geo.lat).toPython()
             lon = graph.value(subject, Geo.long).toPython()
             return lat, lon
         elif (subject, OxPoints.primaryPlace, None) in graph:
             place = graph.value(subject, OxPoints.primaryPlace)
-            depth += 1
             return find_location(graph, place, depth=depth)
         elif (subject, SpatialRelations.within, None) in graph:
             within = graph.value(subject, SpatialRelations.within)
-            depth += 1
             return find_location(graph, within, depth=depth)
         elif (subject, Org.subOrganizationOf, None) in graph:
             org = graph.value(subject, Org.subOrganizationOf)
-            depth += 1
             return find_location(graph, org, depth=depth)
     return None
